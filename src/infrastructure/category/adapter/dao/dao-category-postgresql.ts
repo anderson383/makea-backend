4b81c9049833696721d5918ade60effad90f2485
Anderson Vargas Sepulveda <andersonvargas383@gmail.com>
2023-05-07 00:52:44 -0500

    fix conection database

--- src/infrastructure/category/adapter/dao/dao-category-postgresql.ts
+++ src/infrastructure/category/adapter/dao/dao-category-postgresql.ts
@@ -4,13 +4,11 @@ import { DaoCategory } from '../../../../domain/category/dao/dao-category';
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/infrastructure/config/prisma-service';
 
-
 @Injectable()
 export class DaoCategoryPostgresql implements DaoCategory {
   constructor(private prisma: PrismaService) {
 
   }
-
 
   async listProductsHistory(id: string): Promise<any> {
 
